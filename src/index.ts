ee1b2eb2431b82ed3ef97f32b10dbdc0fee67497
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-01-11 18:30:04 -0500

    Dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
 import core from "@actions/core"
-import github from "@actions/github"
+import github from "@actions/github";
+import fs from "fs";
+import path from "path";
+
+const parseInputs = (env: NodeJS.ProcessEnv) => {
+  const sourceFiles = env.INPUT_SOURCE_FILES;
+  const output = env.INPUT_OUTPUT;
+  if (!sourceFiles) {
+    core.setFailed("input: 'source_files' is required!")
+    return undefined;
+  }
+  const sourcePaths: string[] = sourceFiles.trim().split(" ").map(p => p.trim());
+  for (const path of sourcePaths) {
+    if (path.split(".").at(-1) !== "typ") {
+      core.setFailed("input: 'source_files' must be '.typ' files!")
+      return undefined;
+    }
+  }
+  
+  let outputPaths: string[];
+  if (!output) {
+    outputPaths = sourcePaths.map(p => p.split(".")[0] + ".pdf")
+  } else {
+    outputPaths = output.trim().split(" ").map(p => p.trim());
+    if (outputPaths.length !== sourcePaths.length) {}
+  }
+}
 
 try {
   console.log("Hello world!");
   console.log(process.env);
+  console.log(fs.readdirSync(path.join(__dirname)));
 } catch (error) {
   core.setFailed(`Action failed with error: ${error}`);
 }
