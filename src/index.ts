e6da318fe1ed21a17ea29137e6c3b4fb71ae8c29
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-01-11 23:40:09 -0500

    Testing compilation.

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,64 @@
-import core from "@actions/core"
-import github from "@actions/github";
 import fs from "fs";
 import path from "path";
 import { exec, execSync } from "child_process";
 
 const REPO_PATH = path.join(__dirname, "../github/workspace");
 
-const parseInputs = (env: NodeJS.ProcessEnv): [string[], string[]] | undefined => {
-  const sourceFiles = env.INPUT_SOURCE_FILES;
-  const output = env.INPUT_OUTPUT;
-  if (!sourceFiles) {
-    core.setFailed("input: 'source_files' is required!")
-    return undefined;
-  }
-  const sourcePaths: string[] = sourceFiles.trim().split(" ").map(p => p.trim());
-  for (const p of sourcePaths) {
-    if (p.split(".").at(-1) !== "typ") {
-      core.setFailed("input: 'source_files' must be '.typ' files!")
-      return undefined;
-    }
-  }
+// const parseInputs = (env: NodeJS.ProcessEnv): [string[], string[]] | undefined => {
+//   const sourceFiles = env.INPUT_SOURCE_FILES;
+//   const output = env.INPUT_OUTPUT;
+//   if (!sourceFiles) {
+//     core.setFailed("input: 'source_files' is required!")
+//     return undefined;
+//   }
+//   const sourcePaths: string[] = sourceFiles.trim().split(" ").map(p => p.trim());
+//   for (const p of sourcePaths) {
+//     if (p.split(".").at(-1) !== "typ") {
+//       core.setFailed("input: 'source_files' must be '.typ' files!")
+//       return undefined;
+//     }
+//   }
 
-  for (const p of sourcePaths) {
-    const fullPath = path.join(REPO_PATH, p);
-    if (!fs.existsSync(fullPath)) {
-      core.setFailed(`file: '${p}' does not exist!`)
-      return undefined;
-    }
-  }
+//   for (const p of sourcePaths) {
+//     const fullPath = path.join(REPO_PATH, p);
+//     if (!fs.existsSync(fullPath)) {
+//       core.setFailed(`file: '${p}' does not exist!`)
+//       return undefined;
+//     }
+//   }
   
-  let outputPaths: string[];
-  if (!output) {
-    outputPaths = sourcePaths.map(p => {
-      let temp = p.split(".");
-      temp[temp.length - 1] = "pdf"
-      return temp.join(".");
-    })
-  } else {
-    outputPaths = output.trim().split(" ").map(p => p.trim());
-    if (outputPaths.length !== sourcePaths.length) {
-      core.setFailed("Number of output paths must match source files!")
-      return undefined;
-    }
-  }
+//   let outputPaths: string[];
+//   if (!output) {
+//     outputPaths = sourcePaths.map(p => {
+//       let temp = p.split(".");
+//       temp[temp.length - 1] = "pdf"
+//       return temp.join(".");
+//     })
+//   } else {
+//     outputPaths = output.trim().split(" ").map(p => p.trim());
+//     if (outputPaths.length !== sourcePaths.length) {
+//       core.setFailed("Number of output paths must match source files!")
+//       return undefined;
+//     }
+//   }
 
-  return [sourcePaths, outputPaths]
-}
+//   return [sourcePaths, outputPaths]
+// }
 
 (() => {
   try {
     console.log("Hello world!");
     console.log(process.env);
+    const PATH = path.join(__dirname, "../repo")
     console.log(fs.readdirSync(path.join(__dirname, "../repo")));
+    const file1 = path.join(PATH, "./testing/file1.typ")
+    const output = path.join(PATH, "output.pdf")
+    try {
+      execSync(`typst compile ${file1} ${output}`)
+      fs.readdirSync(PATH);
+    } catch (error) {
+      
+    }
     // throw new Error("Test error!")
     // const parsed = parseInputs(process.env);
     // if (!parsed) return;
@@ -67,6 +74,7 @@ const parseInputs = (env: NodeJS.ProcessEnv): [string[], string[]] | undefined =
     //   }
     // }
   } catch (error) {
-    core.setFailed(`Action failed with error: ${error}`);
+    console.log("ERROR:", error);
+    // core.setFailed(`Action failed with error: ${error}`);
   }
 })()
