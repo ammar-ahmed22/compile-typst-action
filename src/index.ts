959471ead890ab0a3584b51718942e06011d5de7
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-01-13 01:45:02 -0500

    Testing workflow

--- src/index.ts
+++ src/index.ts
@@ -5,59 +5,22 @@ import * as exec from "@actions/exec"
 import * as github from "@actions/github"
 import { execSync } from "child_process";
 
-// const parseInputs = (env: NodeJS.ProcessEnv): [string[], string[]] | undefined => {
-//   const sourceFiles = env.INPUT_SOURCE_FILES;
-//   const output = env.INPUT_OUTPUT;
-//   if (!sourceFiles) {
-//     core.setFailed("input: 'source_files' is required!")
-//     return undefined;
-//   }
-//   const sourcePaths: string[] = sourceFiles.trim().split(" ").map(p => p.trim());
-//   for (const p of sourcePaths) {
-//     if (p.split(".").at(-1) !== "typ") {
-//       core.setFailed("input: 'source_files' must be '.typ' files!")
-//       return undefined;
-//     }
-//   }
-
-//   for (const p of sourcePaths) {
-//     const fullPath = path.join(REPO_PATH, p);
-//     if (!fs.existsSync(fullPath)) {
-//       core.setFailed(`file: '${p}' does not exist!`)
-//       return undefined;
-//     }
-//   }
-  
-//   let outputPaths: string[];
-//   if (!output) {
-//     outputPaths = sourcePaths.map(p => {
-//       let temp = p.split(".");
-//       temp[temp.length - 1] = "pdf"
-//       return temp.join(".");
-//     })
-//   } else {
-//     outputPaths = output.trim().split(" ").map(p => p.trim());
-//     if (outputPaths.length !== sourcePaths.length) {
-//       core.setFailed("Number of output paths must match source files!")
-//       return undefined;
-//     }
-//   }
-
-//   return [sourcePaths, outputPaths]
-// }
+const parseInputs = (sourcePaths: string, outputPaths: string, fontsPath: string) => {
+  console.log({ sourcePaths, outputPaths, fontsPath });
+}
 
 
 
 (async () => {
   try {
-    // console.log("Hello world!");
+
     const PATH = path.join(__dirname, "../../github/workspace");
     
     const sourcePaths = core.getInput("source_paths");
     const outputPaths = core.getInput("output_paths", { required: false });
-
+    const fontsPaths = core.getInput("fonts_path", { required: false });
     
-
+    parseInputs(sourcePaths, outputPaths, fontsPaths);
     
     const file1 = path.join(PATH, "./testing/file1.typ")
     const output = path.join(PATH, "output.pdf")
